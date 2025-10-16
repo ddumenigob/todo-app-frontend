b50672e7f87a28cc10413f88aede4ef6436440ba
ddumenigob <98914623+ddumenigob@users.noreply.github.com>
2025-10-16 10:11:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default function TodoApp() {
       setUser(JSON.parse(userData));
       fetchTodos(token);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchTodos = async (token) => {
@@ -275,6 +276,7 @@ export default function TodoApp() {
     </div>
   );
 }
+
 
 
 
